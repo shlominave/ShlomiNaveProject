9e53786075f069d9117aaedaa9580cd0cd29b8cf
shlominave <s1170@motzkine.ort.org.il>
2020-10-22 22:22:03 +0300

    22.10

--- app/src/main/java/com/example/shlominaveproject/MainActivity.java
+++ app/src/main/java/com/example/shlominaveproject/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.shlominaveproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
 import android.os.Bundle;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+      setContentView(R.layout.activity_main);
+    }
+    public void Login(View view) {
+        Intent i=new Intent(this,Login_Activity.class);
+        startActivity(i);
     }
 }
