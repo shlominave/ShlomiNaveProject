2ce471d24b565d9c59e0de3e737f3a726e32d460
shlominave <s1170@motzkine.ort.org.il>
2021-01-04 13:20:56 +0200

    update 04.01

--- app/src/main/java/com/example/shlominaveproject/Login_Activity.java
+++ app/src/main/java/com/example/shlominaveproject/Login_Activity.java
@@ -5,15 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login_Activity extends AppCompatActivity {
+EditText username;
+EditText email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_);
-    }
+        username=findViewById(R.id.tvusername);
+         email=findViewById(R.id.tvemail);
 
+    }
+    public void LoginToast(View v){
+         email.setText("hhhhpopklopp");
+        if(username.getText()==null || email.getText()==null) {
+           email.setText("HHHHIIOOO");
+            Toast.makeText(this,"enter email and/or password", Toast.LENGTH_SHORT).show();
+        }
+        else
+            Toast.makeText(this,"username:"+username.getText().toString()+",email:"+email.getText().toString(), Toast.LENGTH_SHORT);
+}
     public void Create_A_Task(View view) {
         Intent i = new Intent(this, CreateTask_Activity.class);
         startActivity(i);
